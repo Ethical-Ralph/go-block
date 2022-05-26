8817efb22e1750e49d8887b863f4159d64a904fc
EthicalRalph <rakinola90@gmail.com>
2022-05-26 16:47:42 +0100

    blockchain nodes

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Ethical-Ralph/go-block/utils"
+)
+
+func main() {
+	// fmt.Println(utils.FindNeighbors("127.0.0.1", 5000, 0, 3, 5000, 5003))
+	fmt.Println(utils.GetHost())
+}
